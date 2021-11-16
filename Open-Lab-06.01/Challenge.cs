260bd1e9344948fce7d50fc1d40c3add89497221
ToboLestGoIT <tobo1369@gmail.com>
2021-11-16 09:22:54 +0100

    update

--- Open-Lab-06.01/Challenge.cs
+++ Open-Lab-06.01/Challenge.cs
@@ -4,9 +4,17 @@ namespace Open_Lab_06._01
 {
     public class Challenge
     {
-        public string MysteryFunc(string str)
-        {
-            throw new NotImplementedException();
-        }
+         public string MysteryFunc(string str)
+         {
+               string temp_string = "";
+               for (int i = 0; i < str.Length; i++)
+               {
+                    if ("0123456789".Contains(str[i]))
+                    {
+                        for (int x = 0; x < int.Parse(str[i].ToString()); x++){temp_string += str[(i - 1)];}
+                    }
+               }
+               return temp_string;
+         }
     }
 }
